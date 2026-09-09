73891d47f75d18df3bce932ae905df130007df1a
Shan0n73 <agrisyandimaraja@gmail.com>
2026-09-09 16:32:18 +0700

    Percobaan 2 (Penambahan Objek)

--- Js2Pt2/TestMahasiswa.java
+++ Js2Pt2/TestMahasiswa.java
@@ -2,6 +2,7 @@ public class TestMahasiswa {
     public static void main(String[] args){
         Mahasiswa mhs1 = new Mahasiswa();
         Mahasiswa mhs2 = new Mahasiswa();
+        Mahasiswa mhs3 = new Mahasiswa();
 
         mhs1.nim = 101;
         mhs1.nama = "Cornelius B.";
@@ -14,5 +15,11 @@ public class TestMahasiswa {
         mhs2.alamat = "Jl. Soekarno Hatta No. 12";
         mhs2.kelas = "B";
         mhs2.tampilBiodata();
+
+        mhs3.nim = 103;
+        mhs3.nama = "Giorno G.";
+        mhs3.alamat = "Jl. Diponegoro No. 34";
+        mhs3.kelas = "C";
+        mhs3.tampilBiodata();
     }
 }
