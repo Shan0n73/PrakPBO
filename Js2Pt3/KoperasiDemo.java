760f25ce7cefbf0176197e026e0963ed010d7e7a
Shan0n73 <agrisyandimaraja@gmail.com>
2026-09-11 09:55:30 +0700

    Percobaan 3

---
+++ Js2Pt3/KoperasiDemo.java
@@ -0,0 +1,12 @@
+public class KoperasiDemo {
+    public static void main(String[] args) {
+        Anggota anggota1 = new Anggota();
+        anggota1.setNama("Lihanna von Arkheim");
+        anggota1.setAlamat("Chekshino, Rusia");
+        anggota1.setor(3000000);
+        System.out.println("Simpanan " + anggota1.getNama()+ ": Rp. "+anggota1.getSimpanan());
+
+        anggota1.pinjam(250000);
+        System.out.println("Simpanan " + anggota1.getNama()+ ": Rp. "+anggota1.getSimpanan());
+    }
+}
