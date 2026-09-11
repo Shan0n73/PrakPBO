cc530135cb6c648da2bf48cc980ba905aee47368
Shan0n73 <agrisyandimaraja@gmail.com>
2026-09-11 09:56:45 +0700

    Percobaan 4

--- Js2Pt3/KoperasiDemo.java
+++ Js2Pt3/KoperasiDemo.java
@@ -1,6 +1,8 @@
 public class KoperasiDemo {
     public static void main(String[] args) {
-        Anggota anggota1 = new Anggota();
+        Anggota anggota1 = new Anggota("Lihanna","Chekshino");
+        System.out.println("Simpanan " + anggota1.getNama()+ ": Rp. "+anggota1.getSimpanan());
+
         anggota1.setNama("Lihanna von Arkheim");
         anggota1.setAlamat("Chekshino, Rusia");
         anggota1.setor(3000000);
