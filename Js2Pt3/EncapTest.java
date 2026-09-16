bbbbe6605ea560bda185c66183e16ec12996a678
Shan0n73 <agrisyandimaraja@gmail.com>
2026-09-16 16:42:14 +0700

    Tugas Praktik 1-3 (Modifikasi)

--- Js2Pt3/EncapTest.java
+++ Js2Pt3/EncapTest.java
@@ -2,11 +2,27 @@
 public class EncapTest {
 
     public static void main(String args[]) {
-        EncapDemo encap = new EncapDemo();
-        encap.setName("James");
-        encap.setAge(35);
-        System.out.println("Name: " + encap.getName());
-        System.out.println("Age: " + encap.getAge());
+        EncapDemo encap1 = new EncapDemo();
+        EncapDemo encap2 = new EncapDemo();
+        EncapDemo encap3 = new EncapDemo();
+        
+        encap1.setName("Xenon");
+        encap1.setAge(40);
+
+        encap2.setName("Diego");
+        encap2.setAge(21);
+
+        encap3.setName("Rachel");
+        encap3.setAge(17);
+
+        System.out.println("Name: " + encap1.getName());
+        System.out.println("Age: " + encap1.getAge());
+
+        System.out.println("Name: " + encap2.getName());
+        System.out.println("Age: " + encap2.getAge());
+
+        System.out.println("Name: " + encap3.getName());
+        System.out.println("Age: " + encap3.getAge());
 
     }
 }
