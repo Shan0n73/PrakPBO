81cc61d12dc58f1a244de13fb09168159aa5be16
Shan0n73 <agrisyandimaraja@gmail.com>
2026-09-23 19:25:09 +0700

    Percobaan 3

---
+++ Js4Pt4/Pc3/MainPc3.java
@@ -0,0 +1,12 @@
+package Js4Pt4.Pc3;
+
+public class MainPc3 {
+
+    public static void main(String[] args) {
+        Pegawai masinis = new Pegawai("1234", "Spongebob Squarepants");
+        Pegawai asisten = new Pegawai("4567", "Patrick Star");
+        KeretaApi keretaApi = new KeretaApi("Gaya Baru", "Bisnis", masinis, asisten);
+
+        System.out.println(keretaApi.info());
+    }
+}
---
+++ Js4Pt4/Pc3/MainPertanyaan.java
@@ -0,0 +1,11 @@
+package Js4Pt4.Pc3;
+
+public class MainPertanyaan {
+
+    public static void main(String[] args) {
+        Pegawai masinis = new Pegawai("1234", "Spongebob Squarepants");
+        KeretaApi keretaApi = new KeretaApi("Gaya Baru", "Bisnis", masinis);
+
+        System.out.println(keretaApi.info());
+    }
+}
