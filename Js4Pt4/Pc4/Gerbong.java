8f5b3851cbf96c01ce8eadeb2db358d49b21a3a5
Shan0n73 <agrisyandimaraja@gmail.com>
2026-09-23 20:06:13 +0700

    Percobaan 4 - Modifikasi agar objek tidak bisa menimpa data dari objek lainnya + Add gerbong

--- Js4Pt4/Pc4/Gerbong.java
+++ Js4Pt4/Pc4/Gerbong.java
@@ -18,7 +18,11 @@ public class Gerbong {
     }
 
     public void setPenumpang(Penumpang penumpang, int nomor) {
-        this.arrayKursi[nomor - 1].setPenumpang(penumpang);
+        if (this.arrayKursi[nomor - 1].getPenumpang() != null) {
+            System.out.println("Kursi nomor " + nomor + " sudah diisi oleh " + this.arrayKursi[nomor - 1].getPenumpang().getNama() + "!");
+        } else {
+            this.arrayKursi[nomor - 1].setPenumpang(penumpang);
+        }
     }
 
     public String info() {
--- Js4Pt4/Pc4/MainPc4.java
+++ Js4Pt4/Pc4/MainPc4.java
@@ -4,13 +4,17 @@ public class MainPc4 {
 
     public static void main(String[] args) {
         Penumpang p1 = new Penumpang("12345", "Mr. Krab");
-        Penumpang p2 = new Penumpang("99999", "Budi");
+        Penumpang p2 = new Penumpang("18732", "Van Helsing");
+        Penumpang p3 = new Penumpang("23415", "Knox von Reinhaver");
 
-        Gerbong gerbong = new Gerbong("A", 10);
-        gerbong.setPenumpang(p1, 1);
-        gerbong.setPenumpang(p2, 1);
+        Gerbong gerbong1 = new Gerbong("A", 10);
+        gerbong1.setPenumpang(p1, 1);
+        gerbong1.setPenumpang(p2, 2);
 
+        Gerbong gerbong2 = new Gerbong("B",10);
+        gerbong2.setPenumpang(p3, 1);
 
-        System.out.println(gerbong.info());
+        System.out.println(gerbong1.info());
+        System.out.println(gerbong2.info());
     }
 }
